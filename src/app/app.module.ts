53cae10f738c4ed58a7a12f28976e1970cdcc0b5
John Patrick Agustin <agustin.johnpatrick@gmail.com>
2017-04-12 18:01:59 +0800

    With Firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,20 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+// Firebase Configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBGJ0zckz8TB0CYGoNPZQKxjAc7t1bddKg",
+  authDomain: "misyon-9b85c.firebaseapp.com",
+  databaseURL: "https://misyon-9b85c.firebaseio.com",
+  projectId: "misyon-9b85c",
+  storageBucket: "misyon-9b85c.appspot.com",
+  messagingSenderId: "815240287928"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +26,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
